cc8013b14a30a91bf575f3de6946a9f61fc73df0
Nihir Zala <zalanihir30@gmail.com>
2023-09-27 18:08:40 +0530

    update the login request

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -2,12 +2,4 @@
     @section('title')
         {{'Dashboard'}}
     @endsection
-
-    <div class="card">
-        <div class="card-body">
-            <div class="p-3">
-                {{ __("You're logged in!") }}
-            </div>
-        </div>
-    </div>
 </x-admin>
