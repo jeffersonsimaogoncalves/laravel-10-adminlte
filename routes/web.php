b5f4e4a6de087a818169f47514bbe336175ab705
Nihir Zala <zalanihir30@gmail.com>
2023-09-27 17:53:59 +0530

    created user module

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,9 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
-
+use App\Models\User;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -19,9 +20,16 @@ Route::get('/', function () {
 });
 require __DIR__.'/auth.php';
 
+
+// Admin Routes
+
 Route::prefix('admin')->name('admin.')->middleware(['auth', 'verified'])->group(function () {
+    $userCount = User::count();
+    view()->share('userCount',$userCount);
     Route::get('/dashboard',[ProfileController::class,'dashboard'])->name('dashboard');
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::resource('user',UserController::class);
 });
