37eb4889c2e0001fce39957480a2a92055949c3c
Nihir Zala <zalanihir30@gmail.com>
2023-09-27 15:47:25 +0530

    update the env changes

--- resources/views/components/admin.blade.php
+++ resources/views/components/admin.blade.php
@@ -24,7 +24,7 @@
     <!-- Brand Logo -->
     <a href="javascript:void(0)" class="brand-link">
       <img src="{{ asset('admin/dist/img/AdminLTELogo.png') }}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-      <span class="brand-text font-weight-light">AdminLTE 3</span>
+      <span class="brand-text font-weight-light">{{ env('APP_NAME') }}</span>
     </a>
 
     <!-- Sidebar -->
