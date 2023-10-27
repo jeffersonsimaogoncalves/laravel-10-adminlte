e53c4944cc1793f0b33a167cdf9594ec280e7a3a
Nihir Zala <zalanihir30@gmail.com>
2023-10-27 17:51:40 +0530

    added the alerts & the alerts

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -2,9 +2,12 @@
     @section('title')
         {{ 'Dashboard' }}
     @endsection
+    <div class="alert alert-success alert-dismissible fade show" role="alert">
+        <strong>Congratulations !</strong> You loged in successfully.
+        <button type="button" class="btn btn-sm float-end float-right" data-bs-dismiss="alert" aria-label="Close">&times;</button>
+      </div>
     <div class="row">
         <div class="col-lg-3 col-6">
-
             <div class="small-box bg-info">
                 <div class="inner">
                     <h3>150</h3>
