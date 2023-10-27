f742db3dedf9b5e8f463b2c9a4a865ec20694bf3
Nihir Zala <zalanihir30@gmail.com>
2023-10-27 18:01:40 +0530

    added the new roled in database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,9 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
+
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,11 +16,19 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // \App\Models\User::factory(10)->create();
+        $admin = Role::create(['name' => 'admin']);
+        $user = Role::create(['name' => 'user']);
 
         \App\Models\User::factory()->create([
-            'name' => 'Test User',
+            'name' => 'Test Admin',
             'email' => 'testnihir@gmail.com',
             'password' => bcrypt('password'),
-        ]);
+        ])->assignRole($admin);
+        
+        \App\Models\User::factory()->create([
+            'name' => 'Test User',
+            'email' => 'testuser@gmail.com',
+            'password' => bcrypt('password'),
+        ])->assignRole($user);
     }
 }
