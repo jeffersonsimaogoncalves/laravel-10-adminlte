1e8ad0dcb8c2d1d6b6d806841db8729e16ebd768
Nihir Zala <zalanihir30@gmail.com>
2023-11-07 18:27:36 +0530

    update the database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\UiMode;
 use Illuminate\Database\Seeder;
 use Spatie\Permission\Models\Role;
 use Spatie\Permission\Models\Permission;
@@ -15,7 +17,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        $this->call(CountryStateCityTableSeeder  ::class);
+        $this->call(CountryStateCityTableSeeder::class);
         $admin = Role::create(['name' => 'admin']);
         $user = Role::create(['name' => 'user']);
 
@@ -30,5 +32,9 @@ class DatabaseSeeder extends Seeder
             'email' => 'testuser@gmail.com',
             'password' => bcrypt('password'),
         ])->assignRole($user);
+
+        UiMode::crate([
+            'mode'=>'light',
+        ]);
     }
 }
