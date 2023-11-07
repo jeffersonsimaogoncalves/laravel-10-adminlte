6b82f3dcadce0ac7c612f736344a15093c61ff05
Nihir <testnihir@gmail.com>
2023-11-07 20:21:30 +0530

    update the admin side title section

--- resources/views/components/admin.blade.php
+++ resources/views/components/admin.blade.php
@@ -13,7 +13,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Admin | {{ env('APP_NAME') }}</title>
+    <title>Admin | {{ config('app.name') }}</title>
 
     <!-- Google Font: Source Sans Pro -->
     <link rel="stylesheet"
